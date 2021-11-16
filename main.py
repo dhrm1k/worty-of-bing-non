32ae34b1c3da5eeb172e788921846348c70f90e6
Dharmik2004 <dharmikparmar2004@yahoo.com>
2021-11-17 00:05:49 +0530

    Wish I could hold you, it's only you In the moments when I walk these city streets with someone else I wish I told you, it's only you

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
 from replit import db, web
 import json
+import requests
 
 app = flask.Flask(__name__)
 app.static_url_path = "/static"
@@ -11,26 +12,25 @@ app.static_url_path = "/static"
 def index():
   thoughts = flask.request.form.get("thoughts")
 
+
+
   strthoughts = str(thoughts)
 
     
   entry = strthoughts
+  if entry != "None":
+    filename = 'posts.json'
 
+      # 1. Read file contents
+    with open(filename, "r") as file:
+      data = json.load(file)
+      data['thoughts'].insert(0, entry)
+
+      # 3. Write json file
+    with open(filename, "w") as file:
+      json.dump(data, file)
   
 
-  filename = 'posts.json'
-
-
-  # 1. Read file contents
-  with open(filename, "r") as file:
-    data = json.load(file)
-    data.append(entry)
-
-# 3. Write json file
-  with open(filename, "w") as file:
-    json.dump(data, file)
-
-  
 
   return flask.render_template('index.html', thoughts=thoughts)
 
